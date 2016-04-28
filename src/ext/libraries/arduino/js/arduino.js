a9328a3fd0599c78851671c2bb47b33faec33c81
snjdck <86191770@qq.com>
2016-04-28 12:25:43 +0800

    fix getPackage() error

--- src/ext/libraries/arduino/js/arduino.js
+++ src/ext/libraries/arduino/js/arduino.js
@@ -98,35 +98,28 @@
 		}
 		responseValue(nextID,new Date().getTime()-startTimer);
 	}
-	
-	function runPackage(){
-		var bytes = [];
-		bytes.push(0xff);
-		bytes.push(0x55);
-		bytes.push(0);
-		bytes.push(0);
-		bytes.push(2);
-		for(var i=0;i<arguments.length;i++){
-			if(arguments[i].constructor == "[class Array]"){
-				bytes = bytes.concat(arguments[i]);
+
+	function sendPackage(argList, type){
+		var bytes = [0xff, 0x55, 0, 0, type];
+		for(var i=0;i<argList.length;++i){
+			var val = argList[i];
+			if(val.constructor == "[class Array]"){
+				bytes = bytes.concat(val);
 			}else{
-				bytes.push(arguments[i]);
+				bytes.push(val);
 			}
 		}
-		bytes[2] = bytes.length-3;
+		bytes[2] = bytes.length - 3;
 		device.send(bytes);
+	}
+	
+	function runPackage(){
+		sendPackage(arguments, 2);
 	}
 	function getPackage(){
 		var nextID = arguments[0];
-
-		var bytes = [0xff, 0x55];
-		bytes.push(arguments.length+1);
-		bytes.push(0);
-		bytes.push(1);
-		for(var i=1;i<arguments.length;i++){
-			bytes.push(arguments[i]);
-		}
-		device.send(bytes);
+		var argList = Array.prototype.slice.call(arguments, 1);
+		sendPackage(argList, 1);
 	}
 
     var inputArray = [];
