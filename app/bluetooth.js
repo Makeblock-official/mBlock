14854f8bc34cc66c46a6a587e14f03175c8545e2
Bear <258333309@163.com>
2017-01-21 10:39:32 +0800

    完美解决蓝牙连接和断开问题

--- app/bluetooth.js
+++ app/bluetooth.js
@@ -1,57 +1,22 @@
 /**
  * 蓝牙串口通讯
+ * @author Bear
  */
- 
-/*var btSerial = new (require('bluetooth-serial-port')).BluetoothSerialPort();
- 
-btSerial.on('found', function(address, name) {
-    btSerial.findSerialPortChannel(address, function(channel) {
-        btSerial.connect(address, channel, function() {
-            console.log('connected');
- 
-            btSerial.write(new Buffer('my data', 'utf-8'), function(err, bytesWritten) {
-                if (err) console.log(err);
-            });
- 
-            btSerial.on('data', function(buffer) {
-                console.log(buffer.toString('utf-8'));
-            });
-        }, function () {
-            console.log('cannot connect');
-        });
- 
-        // close the connection when you're ready 
-        btSerial.close();
-    }, function() {
-        console.log('found nothing');
-    });
-});
- 
-btSerial.inquire();*/
- 
-const {MenuItem} = require("electron")
-//const SPP = require('bluetooth-serial-port');
+const {MenuItem} = require("electron");
 const events = require('events');
+const childProcess = require('child_process'); // 子进程
 var _emitter = new events.EventEmitter();  
 var _app,_client,_items=[];
 var _devices = {}; // 缓存中[Cookie]的蓝牙设备
 var _currentBluetooth = '';
-
-
-
-const childProcess = require('child_process'); // 子进程
 var bluetoothChildProcess; // 蓝牙子进程
-
-
 
 function Bluetooth(app){
     var self = this;
     _app = app;
 	_client = _app.getClient();
 	var _translator = _app.getTranslator(); // 多语言类
-	
 
-	
     /**
      * 是否已连接
      */
@@ -74,18 +39,23 @@ function Bluetooth(app){
 		bluetoothChildProcess.send({'method':'connect', 'device':device}); 
     };
 	
-    this.close = function () { // 断开蓝牙连接
+    this.close = function (isUpdateMenu) { // 断开蓝牙连接
 	    _currentBluetooth = '';
+		if (typeof(isUpdateMenu) == 'undefined') {
+			isUpdateMenu = true;
+		}
         if (typeof(bluetoothChildProcess) != 'undefined') {
-            self.killBluetoothChildProcess();
 			self.onDisconnect();
-		} else {
-            self.updateMenu(); // 需要更新菜单
+		}
+		if (isUpdateMenu) {
+			self.updateMenu(); // 需要更新菜单
 		}
     };
+	
     this.send = function(data){ // 向蓝牙发送数据
         bluetoothChildProcess.send({'method':'writeData', 'data':data});
-    }
+    };
+	
 	this.foundBluetooth = function (device) { // 找到一个蓝牙设备
         _devices[device.address] = {
 			'label'   : device.label,
@@ -94,12 +64,11 @@ function Bluetooth(app){
 		};
         _app.getLocalStorage().setCookie('bluetoothDevices', _devices);
 	};
-	this.createBluetoothChildProcess = function () {
+	
+	this.createBluetoothChildProcess = function () { // 创建蓝牙子进程，并托管各消息处理函数
 		bluetoothChildProcess = childProcess.fork(__root_path + '/app/bluetoothChildProcess.js');
 		// 监控所有子进程过来的消息
-		bluetoothChildProcess.on('message', function (message) { // (m,cor) => {}
-			console.log(`from worker[child process] message:`);
-			console.log(message);
+		bluetoothChildProcess.on('message', function (message) {
 			if (message.method == 'noBluetoothDevices') { // 周围未找到任何蓝牙设备或最后一个蓝牙设备未找到通道
                 self.updateMenu(); // 更新菜单
 				if (message.isAlertMessage) {
@@ -108,11 +77,9 @@ function Bluetooth(app){
 				}
 				// 关闭子进程
 				self.killBluetoothChildProcess();
-				console.log('周围未找到任何蓝牙设备或最后一个蓝牙设备未找到通道,且已更新菜单');
 			} else if (message.method == 'foundBluetooth') { // 找到一个蓝牙设备
 				self.foundBluetooth(message.device);
 			} else if (message.method == 'finishedBluetooth') { // 已完成蓝牙设备的查找
-			    console.log('已完成蓝牙设备的查找，准备更新菜单');
 				self.updateMenu(); // 更新菜单
 				// 关闭子进程
 				self.killBluetoothChildProcess();
@@ -121,53 +88,47 @@ function Bluetooth(app){
 			} else if (message.method == 'onConnected') { // 进行了连接蓝牙
 				if (message.isConnected) { // 已连接成功
 					self.onOpen(message.address);
+				} else { // 未连接成功
+					self.onDisconnect();
+					_currentBluetooth = '';
+					self.updateMenu();
 				}
-				self.updateMenu();console.log('已进行过连接，且已更新菜单');
 			}
 		});
         bluetoothChildProcess.on('exit', function (code) { // 不为0时，为异常退出
-		    console.log('蓝牙子进程已经退出。退出code：');
-			console.log(code);
-			//bluetoothChildProcess = null;
 		});
 	};
+	
 	this.killBluetoothChildProcess = function () { // 杀死子进程
-	console.log(bluetoothChildProcess);
-        bluetoothChildProcess.kill();
-		childProcess.spawn('kill', ['-9', bluetoothChildProcess.pid]);
-		console.log('已杀死子进程');
+        bluetoothChildProcess.kill('SIGKILL');
 	};
+	
     this.discover = function(item){ // 发现蓝牙
         _items = [];
         _devices = {};
-		self.close(); // 断开蓝牙连接
-		//var arduinoProcess = spawn(this.getArduinoExecutable(), arduinoCommandArguments);
+		self.close(false); // 一定要断开蓝牙连接
         self.createBluetoothChildProcess();
         bluetoothChildProcess.send({'method':'inquire'});
-
-		
-		/*try {
-            //self.initBluetoothSerialPort();
-            
-		} catch (e) { // 如果linux没有蓝牙设备，“发现”蓝牙时程序会直接关闭，并且不抛任何异常（貌似内存溢出）
-			console.log('发现蓝牙时，发生了错误：');
-			console.log(e);
-		}*/
-    }
+    };
+	
 	this.getMenuItems = function(){
 		return _items;
-	}
+	};
+	
 	this.on = function(event,listener){
 		_emitter.on(event,listener);
-	}
-	this.onOpen = function(address){
+	};
+	
+	this.onOpen = function(address){ // 蓝牙已连接
 		_currentBluetooth = address;
+		self.updateMenu();
 		if(_client){
 			_client.send("connected",{connected:true});
 		}
-	}
+	};
+	
 	this.onDisconnect = function(){ // 断开连接 close the connection when you're ready
-        self.updateMenu();
+	    self.killBluetoothChildProcess();
 		if(_client){
 			try {
 				_client.send("connected",{connected:false});
@@ -177,12 +138,14 @@ function Bluetooth(app){
 				// "Object has been destroyed" Exception.
 			}
 		}
-	}
-	this.onReceived = function(data){
+	};
+	
+	this.onReceived = function(data){ // 接受蓝牙数据
 		if(_client){
 			_client.send("package",{data:data})
 		}
-	}
+	};
+	
 	this.clear = function() { // 清空蓝牙列表
         _app.getLocalStorage().setCookie('bluetoothDevices', {});
 		_items = [];
@@ -229,7 +192,6 @@ function Bluetooth(app){
             self.updateMenu();
         }
     });
-    
-	
+
 }
 module.exports = Bluetooth;
--- app/bluetoothChildProcess.js
+++ app/bluetoothChildProcess.js
@@ -1,6 +1,7 @@
-
-// 蓝牙子进程，用于发现蓝牙和连接蓝牙
-
+/**
+ * 蓝牙子进程，用于发现蓝牙和连接蓝牙
+ * @author Bear
+ */
 const SPP = require('bluetooth-serial-port');
 var number = 0; // 已找到有通道的蓝牙多少个
 //var arguments; // 主进程传过来的参数
@@ -53,19 +54,14 @@ _btSerial.on('data', function(data) { // 接受数据
 });
 _btSerial.on('closed', function() { // 当蓝牙主动断开时或蓝牙已拔出时，会调用此方法
     console.log('子进程的蓝牙串口已断开连接');
-	
+	process.send({'method':'onConnected', 'isConnected':false});
 });
 _btSerial.on('error',function(err){
-	console.log('蓝牙设备发生错误了：');
-	console.log(err);
 })
 
-	
-process.on('message', function (message) {
-    console.log(`from master message:`);
-	console.log(message);
+
+process.on('message', function (message) { // 主进程传过来的消息
 	if (message.method == 'inquire') {
-        console.log('开始发现蓝牙...');
 		number = 0;
 		_btSerial.inquire(); // 异步发现蓝牙
 	} else if (message.method == 'connect') { // 连接蓝牙
@@ -85,52 +81,8 @@ process.on('message', function (message) {
 });
 
 process.on('SIGTERM', function () {
-	console.log('child process is exit!!!!');
-	//process.exit(0);
 });
-
 
 /*arguments = process.argv.slice(2);
 console.log('created child process and send arguments:');
 console.log(arguments);*/
-
-/*_btSerial.on('found', function(address, name) {
-		console.log('已找到蓝牙:'+name+"("+address+")");
-		_btSerial.findSerialPortChannel(address, function(channel) {
-			if (address != '00:0D:19:02:07:94') { // 8C:C8:F4:10:2B:64
-				return;
-			}console.log(address);
-			_btSerial.connect(address, channel, function() {
-				console.log('connecte(已连接成功)，且已更新菜单');
-	 
-				_btSerial.write(new Buffer('my data', 'utf-8'), function(err, bytesWritten) {
-					if (err) console.log(err);
-				});
-	 
-				_btSerial.on('data', function(buffer) {
-					console.log(buffer.toString('utf-8'));
-				});
-				
-			}, function (error) {
-				console.log('cannot connect:');
-				console.log(error);
-				console.log('连接失败，且已更新菜单');
-			});
-	 
-			// close the connection when you're ready 
-
-			setTimeout(function () {
-				console.log('ready close ');
-				_btSerial.close();console.log('is closed');
-			}, 5000);
-			
-			
-		}, function() {
-			
-			console.log('can\'t found channel');
-		});
-	});
-	
-	_btSerial.on('closed', function() { // 当蓝牙主动断开时或蓝牙已拔出时，会调用此方法
-        console.log('已经关闭！！！');
-	});*/
