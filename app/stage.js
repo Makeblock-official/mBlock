ff60d2714d3cc08827b1396b03dc93bd0f7d2cf3
yanglingqing <yanglingqing@makeblock.com>
2017-01-22 11:57:36 +0800

    1.新建时提示保存 2.舞台模式切换修复

--- app/stage.js
+++ app/stage.js
@@ -18,30 +18,59 @@ function Stage(app){
         _client.send("changeStageMode",{name:"undelete"});
     }
 	this.changeStageMode = function(name){
+		//flash当前已有逻辑：
+		// 1.勾选或者取消 隐藏模式时，同时取消小舞台模式
+		// 2.勾选或者取消 小舞台模式时，如果当前是隐藏模式，则取消隐形模式和取消小舞台模式
+		// 3.arduino mode 取消时，同时取消小舞台和隐藏模式， arduino mode 勾选时，同时勾选小舞台和隐藏模式
+
 		_stageMode[name] = !_stageMode[name];
 		if(name=="arduino mode"){
 			if(_stageMode[name] == false){
-				_stageMode["hide stage layout"] = false;	
+				//取消勾选
+				_stageMode["hide stage layout"] = false;
+				_stageMode["small stage layout"] = false;
+
 			}else{
-				_stageMode["hide stage layout"] = true;	
+				//勾选
+				_stageMode["hide stage layout"] = true;
+				_stageMode["small stage layout"] = false;
 			}
-			_stageMode["small stage layout"] = false;
+			_client.send("changeStageMode",{name:name});
+
 		}else if(name=="small stage layout"){
-			if(_stageMode["hide stage layout"]&&!_stageMode["arduino mode"]){
-				_client.send("changeStageMode",{name:"hide stage layout"});
-			}else if(_stageMode["arduino mode"]&&_stageMode["small stage layout"]){
-				_client.send("changeStageMode",{name:"arduino mode"});
-				_stageMode["arduino mode"] = false;
-			}
-			_stageMode["hide stage layout"] = false;
-		}else if(name=="hide stage layout"){
-			if (_stageMode["small stage layout"]){
+
+			if (_stageMode["hide stage layout"]){
+				//如果当前为隐藏模式，则取消隐藏模式，什么也不做
+				//(取消隐藏模式时，需要一起取消 arduino mode)
+				if (_stageMode['arduino mode']){
+					_client.send("changeStageMode",{name:"arduino mode"}); //取消arduino mode 是，隐藏模式和小舞台模式也会一并取消
+					_stageMode["arduino mode"] = false;
+					_stageMode["hide stage layout"] = false;
+					_stageMode[name] = false;
+				}else{
+					_client.send("changeStageMode",{name:"small stage layout"});
+					_stageMode["hide stage layout"] = false;
+					_stageMode[name] = false;
+				}
+			}else{
 				_client.send("changeStageMode",{name:"small stage layout"});
+			}
+
+
+		}else if(name=="hide stage layout") {
+
+
+			if (_stageMode['arduino mode']) {
+				_client.send("changeStageMode", {name: "arduino mode"}); //取消arduino mode 是，隐藏模式和小舞台模式也会一并取消
+				_stageMode["arduino mode"] = false;
+				_stageMode["hide stage layout"] = false;
+				_stageMode['small stage layout'] = false;
+			}else{
+				_client.send("changeStageMode", {name: "hide stage layout"});
 				_stageMode["small stage layout"] = false;
 			}
 
 		}
-		_client.send("changeStageMode",{name:name});
 		_app.getMenu().update();
 	}
 	this.onlyChangeArduinoStageMode = function(bool){
