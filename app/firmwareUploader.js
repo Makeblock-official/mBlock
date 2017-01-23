fb858b1be900f97ebbcb4737d8eaa239afbc9c1b
Bear <258333309@163.com>
2017-01-23 09:53:21 +0800

    弹框支持是否显示“取消”按钮

--- app/firmwareUploader.js
+++ app/firmwareUploader.js
@@ -117,7 +117,7 @@ var FirmwareUploader = {
         
         var self = this;
         console.log('upgrade firmware');
-        app.alert(T('Uploading...'));
+        app.alert({'message':T('Uploading...'), 'hasCancel':false});
         var command = self.getArduinoPath() + '/hardware/tools/avr/bin/avrdude';
         var args = self.getAvrdudeParameter(serialPort, hexFileName); 
         app.getSerial().close();
@@ -126,7 +126,7 @@ var FirmwareUploader = {
         });
         avrdude.stderr.on('data', function(data){
             app.logToArduinoConsole(data.toString());
-            app.alert(T('Uploading')+'...'+utils.getProgressCharacter()); 
+            app.alert({'message':T('Uploading')+'...'+utils.getProgressCharacter(), 'hasCancel':false});
         });
         avrdude.on('close', function(code){
             if(code == 0) {
--- web/js/libs/ui-components/alertBox.js
+++ web/js/libs/ui-components/alertBox.js
@@ -27,8 +27,23 @@ var AlertBox = {
         });
         return self;
     },
+	/**
+	 * 显示弹框
+	 * @param {string} | {object} content 需要显示的内容，如果为对象，则: {'message':'提示消息，支持html','hasCancel':'是否有取消按钮，默认true:需要“取消”按钮，false：不要“取消”按钮',...}
+	 */
     show: function(content) {
-        this.setContent(content);
+	    var message = '';
+		if (typeof(content) == 'object') { // 传对象
+		    if (typeof(content.hasCancel) != 'undefined') {
+				if (!content.hasCancel) { // 不要取消按钮 
+				    closeButtonDom.style.display = 'none';
+			    }
+			}
+			message = content.message
+		} else { // 兼容之前的直接传字符串
+			message = content;
+		}
+        this.setContent(message);
         if(!dom.open) {
             dom.showModal();
         }
